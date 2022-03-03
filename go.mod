989976a84171d6940c48dfd772652c85bea60619
Houdini <nonsoamadi@aol.com>
2022-03-03 10:59:09 +0100

    :hammer: create test suite

--- go.mod
+++ go.mod
@@ -12,8 +12,13 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/datatypes v1.0.5 // indirect
 	gorm.io/driver/mysql v1.2.2 // indirect
 )
---
+++ tests/utils_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/NonsoAmadi10/echoweb/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestUtils( t *testing.T){
+	t.Run("HashPassword function should has a string of password provided", func(t *testing.T) {
+		password:= "hello-123"
+
+		hash, _ := utils.HashPassword(password)
+
+		assert.NotEqual(t, hash, password)
+	})
+
+	t.Run("ComparePassword should return true when a hash password compares with its original palintext", func(t *testing.T) {
+		password := "yellow"
+		fakepwd := "blue"
+
+		hash, _ := utils.HashPassword(password)
+		
+		compare := utils.CheckPasswordHash(password, hash)
+
+		wrong := utils.CheckPasswordHash(fakepwd, hash)
+
+		assert.Equal(t, true, compare)
+		assert.Equal(t, false, wrong)
+	})
+}
